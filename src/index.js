a2ce546ee25007e7e4ce8e4e6df29914c77538df
Drew Hill <mdude2314@gmail.com>
2017-05-22 17:32:54 -0400

    initial commit. completed file structure. basic react rendering app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyA_VjCFv3ocHUbYNXZnw2SQ-kTPUkfdzUE';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// Create a new component. This component should produce
+// some HTML.
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+const App = () => {
+    return <div>Hi!</div>;
+}
+
+
+// Take this component's generated HTML and put it
+// on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
