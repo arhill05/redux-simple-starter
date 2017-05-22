54c192ed016a95df71eba96cbbdaca8eac1ee5c1
Drew Hill <mdude2314@gmail.com>
2017-05-22 17:47:04 -0400

    convert to class component for searchbar

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 const API_KEY = 'AIzaSyA_VjCFv3ocHUbYNXZnw2SQ-kTPUkfdzUE';
 
 // Create a new component. This component should produce
 // some HTML.
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
 }
 
 
