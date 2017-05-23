55d41b3710e0b8c301bacc170b4307c164c6060f
Drew Hill <mdude2314@gmail.com>
2017-05-22 20:46:44 -0400

    populating a list with data from YouTube's API about surfboards. display in list with image and title

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from'./components/video_list';
 
 const API_KEY = 'AIzaSyA_VjCFv3ocHUbYNXZnw2SQ-kTPUkfdzUE';
 
-// Create a new component. This component should produce
-// some HTML.
+class App extends Component {
 
-const App = () => {
-    return (
-    <div>
-        <SearchBar />
-    </div>
-    );
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: API_KEY,
+            term: 'surfboards'
+        }, (videos) => {
+            this.setState({videos});
+        });
+
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
-
-// Take this component's generated HTML and put it
-// on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render(
+    <App/>, document.querySelector('.container'));
